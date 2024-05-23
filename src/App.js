876367707bd3649f517986926f3b7dad52354fc7
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-23 17:05:12 +0200

    Add message form with submit handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
 
-const App = () => (
-    <></>
-)
+const App = () => {
+    const handleMessageFormSubmit = event => {
+        event.preventDefault()
+
+        const messageInput = event.target.querySelector( 'input' )
+        const message = messageInput.value
+
+        console.log( message )
+
+        messageInput.value = ''
+    }
+
+    return (
+        <>
+            <section>
+                <h1>Messages</h1>
+
+                <ul>
+                    <li>Message 1</li>
+                    <li>Message 2</li>
+                    <li>Message 3</li>
+                </ul>
+            </section>
+
+            <form onSubmit={ handleMessageFormSubmit }>
+                <label>
+                    <span>Message</span>
+                    <input type="text" />
+                </label>
+
+                <button type="submit">Send</button>
+            </form>
+        </>
+    )
+}
 
 export default App
