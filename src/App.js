a7f36ea6f30425a83d67ca2e4b00a6ca55520411
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-23 17:15:18 +0200

    Add messages list management

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
+import { v4 as uuid } from 'uuid'
+
 import './App.css';
+import { useReducer } from 'react';
 
 const App = () => {
+    const [ messages, dispatch ] = useReducer( ( state, action ) => {
+        switch ( action.type ) {
+            case 'upsertMessage': {
+                return state.some( message => message.id === action.message.id )
+                    ? state.map( message => message.id === action.message.id ? action.message : message )
+                    : [ ...state, action.message ]
+            }
+            default: {
+                return state
+            }
+        }
+    }, [] )
+
     const handleMessageFormSubmit = event => {
         event.preventDefault()
 
-        const messageInput = event.target.querySelector( 'input' )
-        const message = messageInput.value
+        const message = {
+            id: uuid(),
+            role: 'user',
+            content: event.target.content.value
+        }
 
-        console.log( message )
+        dispatch( {
+            type: 'upsertMessage',
+            message
+        } )
 
-        messageInput.value = ''
+        event.target.reset()
     }
 
     return (
@@ -18,16 +40,18 @@ const App = () => {
                 <h1>Messages</h1>
 
                 <ul>
-                    <li>Message 1</li>
-                    <li>Message 2</li>
-                    <li>Message 3</li>
+                    { messages.map( message => (
+                        <li key={ message.id }>
+                            <strong>{ message.role }</strong>: { message.content }
+                        </li>
+                    ) ) }
                 </ul>
             </section>
 
             <form onSubmit={ handleMessageFormSubmit }>
                 <label>
                     <span>Message</span>
-                    <input type="text" />
+                    <input type="text" name="content" />
                 </label>
 
                 <button type="submit">Send</button>
