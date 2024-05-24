eef73df6af2293a62096fd6b782ad0076163da58
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-24 16:22:43 +0200

    Add ChatMessageForm component

---
+++ src/Chat/MessageForm.jsx
@@ -0,0 +1,54 @@
+import { v4 as uuid } from "uuid"
+
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid"
+
+const ChatMessageForm = ( {
+    onSubmit
+} ) => {
+    const handleMessageFormSubmit = event => {
+        event.preventDefault()
+
+        const message = {
+            id: uuid(),
+            role: 'user',
+            content: event.target.message.value
+        }
+
+        onSubmit( message )
+    }
+
+    const handleSubmitShortcut = event => {
+        if ( event.key === 'Enter' && ( event.ctrlKey || event.metaKey ) ) {
+            event.preventDefault()
+
+            event.target.form.requestSubmit()
+        }
+    }
+
+    return (
+        <form
+            onSubmit={ handleMessageFormSubmit }
+            className="flex flex-row gap-2"
+        >
+            <input
+                type="text"
+                name="message"
+                placeholder="Type a message..."
+                className="flex-grow border border-gray-300 rounded p-2"
+                autoFocus={ true }
+                onKeyDown={ handleSubmitShortcut }
+            />
+
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-2 rounded"
+            >
+                <PaperAirplaneIcon className="h-6 w-6" />
+
+                <span className="sr-only">Send</span>
+            </button>
+        </form>
+    )
+}
+
+export default ChatMessageForm
