37c92762f63bb651b59e1c419e57ad1f8222756e
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-25 12:19:50 +0200

    Add TailwindCSS plugin for animation-delay utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,6 +9,21 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin( ( { matchUtilities, theme } ) => {
+      matchUtilities(
+        {
+          "animation-delay": ( value ) => {
+            return {
+              "animation-delay": value,
+            };
+          },
+        },
+        {
+          values: theme( 'transitionDelay' ),
+        }
+      );
+    }),
+  ],
 }
 
