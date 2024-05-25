64f3a07311f20c3305cb27c6a421a625b2280825
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-25 10:53:04 +0200

    Add Chat component for real-time chat functionality

---
+++ src/Chat/index.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useReducer } from "react"
+
+import ollama from "ollama/browser"
+import { v4 as uuid } from "uuid"
+
+import ChatMessageList from "./MessageList"
+import ChatMessageForm from "./MessageForm"
+
+const Chat = () => {
+    const [ messages, dispatch ] = useReducer( ( state, action ) => {
+        switch ( action.type ) {
+            case 'upsertMessage': {
+                return state.some( message => message.id === action.message.id )
+                    ? state.map( message => message.id === action.message.id ? action.message : message )
+                    : [ ...state, action.message ]
+            }
+            default: {
+                return state
+            }
+        }
+    }, [] )
+
+    const handleNewUserMessage = message => {
+        dispatch( {
+            type: 'upsertMessage',
+            message
+        } )
+    }
+
+    useEffect( () => {
+        const isLatestMessageFromUser = () => (
+            messages[ messages.length - 1 ]?.role === 'user'
+        )
+
+        const sendMessages = async () => {
+            const responseStream = await ollama.chat( {
+                model: 'llama3',
+                messages: messages.map( message => ( {
+                    role: message.role,
+                    content: message.content
+                } ) ),
+                stream: true
+            } )
+
+            const message = {
+                id: uuid(),
+                role: 'assistant',
+                content: ''
+            }
+
+            for await ( const part of responseStream ) {
+                message.content += part.message.content
+
+                dispatch( {
+                    type: 'upsertMessage',
+                    message
+                } )
+
+                if( part.done === true ) {
+                    break
+                }
+            }
+        }
+
+        if( messages.length && isLatestMessageFromUser() ) {
+            sendMessages()
+        }
+    }, [ messages ] )
+
+    return (
+        <section className="flex flex-col gap-4">
+            <ChatMessageList
+                messages={ messages }
+            />
+
+            <ChatMessageForm
+                onSubmit={ handleNewUserMessage }
+            />
+        </section>
+    )
+}
+
+export default Chat
