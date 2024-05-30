d734d00dbd8649e5f8beb3ca0a9ca19ee1ec35b4
Jakub Burkiewicz <jakub.burkiewicz@conx.eu>
2024-05-30 08:57:35 +0200

    Add the `host` field

--- src/components/Settings/index.jsx
+++ src/components/Settings/index.jsx
@@ -1,6 +1,27 @@
+import { useSettings } from "../../contexts/SettingsContext"
+
 const Settings = () => {
+    const { settings, settingsDispatch } = useSettings()
+
+    const handleHostChange = event => {
+        settingsDispatch( {
+            type: 'setHost',
+            host: event.target.value
+        } )
+    }
+
     return (
         <section className="flex flex-col grow h-full gap-4 p-4">
+            <label className="flex flex-col gap-2">
+                <span className="text-sm">Host</span>
+
+                <input
+                    className="p-2 rounded border border-gray-300 h-10"
+                    type="text"
+                    value={ settings.host }
+                    onChange={ handleHostChange }
+                />
+            </label>
         </section>
     )
 }
